411ab037ff335ba9086d93848ede4b17098dddfd
Philip Buchbender <philip.yannic@gmail.com>
2023-12-19 20:29:55 +0100

    addition to last commit

--- needcode.io-questions/backtracking/40. Combination Sum II.py
+++ needcode.io-questions/backtracking/40. Combination Sum II.py
@@ -26,3 +26,26 @@
 # ]
 
 
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        result = []
+
+        def backtrack(curr, pos, target):
+            if target == 0:
+                result.append(curr[:])
+            if target <= 0:
+                return
+            
+            prev = -1
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev:
+                    continue
+                curr.append(candidates[i])
+                backtrack(curr, i+1, target - candidates[i])
+                curr.pop()
+                prev = candidates[i]
+
+        backtrack([], 0, target)
+        return result
