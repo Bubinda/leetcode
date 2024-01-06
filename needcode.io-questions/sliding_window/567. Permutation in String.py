5f4118e1cc5ec1e41dc6b16787bb52cee693eb68
Philip Buchbender <philip.yannic@gmail.com>
2024-01-06 18:02:16 +0100

    two more sql and pandas questions

--- needcode.io-questions/sliding_window/567. Permutation in String.py
+++ needcode.io-questions/sliding_window/567. Permutation in String.py
@@ -41,3 +41,75 @@ class Solution:
 
         return s1Count == s2Count    
 
+
+
+
+class Solution:
+    def checkInclusion(self, t: str, s: str) -> bool:
+        # get the length of both inputs
+        length_t, length_s = len(t), len(s)
+
+        # if our first text is longer than the second the first (t) cannot be a permutated substring of the second (s)
+        if length_t > length_s: return False
+
+        # create two frequency counter for the window of the string t and the current windows we are cheing in s
+        # they are both consisting of 26 zeros for each letter in the alphabet
+        t_freq,curr_window_freq = [0]*26, [0]*26
+
+        # first we go over the string t to get the t_freq window set and also set the current windo to contain the length of t first chars of s
+        for chars_of_t in range(length_t):
+            # the freqency is set by getting the ordinality of the current read char and the ordinality of the lower case char a 
+            # to determine the position of the character relative to 'a' in the Unicode character set.
+            t_freq[ord(t[chars_of_t]) - ord('a')] += 1
+            curr_window_freq[ord(s[chars_of_t]) - ord('a')] += 1
+
+        # check if wa have a match in our first window
+        if t_freq == curr_window_freq: return True
+
+        # then go over the remaing part of s and check for mathing frequency windows
+        for remaining_chars_of_s in range(length_t, length_s):
+
+            # for each step we add the current char to the freqeuncy window and delete the char that is on length of t position previous of our current char
+            curr_window_freq[ord(s[remaining_chars_of_s]) - ord('a')] += 1
+            curr_window_freq[ord(s[remaining_chars_of_s - length_t]) - ord('a')] -= 1
+
+            # then we check again for a match
+            if t_freq == curr_window_freq: return True
+
+        # if we do not get any mathes we will return False so that there are no permutatiosn of t in s
+        return False
+    
+
+
+# solution by using the counter module -> faster lookups
+from collections import Counter
+
+def checkInclusion(s1, s2):
+    len_s1 = len(s1)
+    len_s2 = len(s2)
+    
+    if len_s1 > len_s2:
+        return False
+    
+    # Count the occurrences of characters in s1
+    counter_s1 = Counter(s1)
+    
+    # Initialize the counter for the first window in s2
+    window = Counter(s2[:len_s1])
+    
+    # Check if the initial window is a permutation of s1
+    if window == counter_s1:
+        return True
+    
+    # Iterate through the rest of the windows in s2
+    for i in range(len_s1, len_s2):
+        # Update the window by adding the new character and removing the first character
+        window[s2[i]] += 1
+        window[s2[i - len_s1]] -= 1
+        
+        # Check if the current window is a permutation of s1
+        if window == counter_s1:
+            return True
+    
+    # No permutation found
+    return False
