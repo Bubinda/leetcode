8dbf03c59f5b7a6c7b514b8858c83bad09c3f4b8
Philip Buchbender <philip.yannic@gmail.com>
2024-01-11 18:37:42 +0100

    more SQL 50 questions

--- leetcode_75/sliding_window/643. Maximum Average Subarray I.py
+++ leetcode_75/sliding_window/643. Maximum Average Subarray I.py
@@ -24,11 +24,19 @@
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        max_avg = sum(nums[:k])
-        curr_sum = sum(nums[:k])
+        
         if len(nums) <= k:
-            max_avg = sum(nums)
+            return sum(nums)/k
+        else:
+            # we initialize the max_avg and the current usm as the sum of the input array from the start up to element with kth index        
+            max_avg = curr_sum = sum(nums[:k])
+
+        # then we run over our input array from index k up tot the end
         for i in range(k,len(nums)):
+            # for each poition we subtrackt the first element and add the new (current element)
             curr_sum += nums[i] - nums[i - k]
+            # then for each index we check if the new curr_sum is larger than the max_avg we have found so far
             max_avg = max(max_avg,curr_sum)
+        # because the window size is always k we can divide by k in the end
+        # previously we just tring to find the largest sum within the windows size of k
         return max_avg/k
